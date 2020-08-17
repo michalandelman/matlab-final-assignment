153a513113b8094cfde7899111f624a1983db0c6
Michal Andelman-Gur <michal.andelman@gmail.com>
2020-08-17 15:42:37 +0300

    refactor Q4

--- hw_final_555863421_302194287_G27781827.m
+++ hw_final_555863421_302194287_G27781827.m
@@ -59,7 +59,7 @@ title('Log total votes per party','fontsize',12);
 
 subplot(2,2,3)
 
-[numbers parties] = maxk(sum(raw_votes),5);
+[numbers, parties] = maxk(sum(raw_votes),5);
 explode = zeros(length(totalvotes_per_party),1)';
 explode(parties) = 1;
 party_labels_to_remove = find(explode==0);
@@ -104,13 +104,13 @@ annotation('textbox',[0.55 0.05 0.4 0.4]...
 % Find the top 10 settlements that had the highest/lowest % valid votes
 
 settlement_names = string(table2array(results(:,1)));
-[votes_max settlement_ind] = maxk(basic_voting_stats(:,4),10);
+[votes_max, settlement_ind] = maxk(basic_voting_stats(:,4),10);
 
 settlement_names_max = settlement_names(settlement_ind);
 fprintf('highest number of valid votes:\n');
 fprintf('%s\n', settlement_names_max);
 
-[votes_min settlement_ind_min] = mink(basic_voting_stats(:,4),10);
+[votes_min, settlement_ind_min] = mink(basic_voting_stats(:,4),10);
 settlement_names_min = settlement_names(settlement_ind_min);
 fprintf('lowest number of valid votes:\n');
 fprintf('%s\n', settlement_names_min);
@@ -125,13 +125,13 @@ raw_votes_with_mean = [raw_votes;mean_votes_per_party];
 correlation_matrix = corr(raw_votes_with_mean');
 num_of_settelments_cor_general = 10;
 
-[correlation_max max_sett_index] = maxk(correlation_matrix(end,1:end-1),...
+[correlation_max, max_sett_index] = maxk(correlation_matrix(end,1:end-1),...
     num_of_settelments_cor_general);
 settlement_names_max_corr = settlement_names(max_sett_index);
 fprintf('highest correlation to general voting pattern:\n');
 fprintf('%s\n', settlement_names_max_corr);
 
-[correlation_min min_sett_index] = mink(correlation_matrix(end,1:end-1),...
+[correlation_min, min_sett_index] = mink(correlation_matrix(end,1:end-1),...
     num_of_settelments_cor_general);
 settlement_names_min_corr = settlement_names(min_sett_index);
 fprintf('lowest correlation to general voting pattern:\n');
@@ -142,13 +142,13 @@ fprintf('%s\n', settlement_names_min_corr);
 mean_removed_matrix = (correlation_matrix(1:end-1,1:end-1));
 lower_triangle = tril(mean_removed_matrix,-1);
 
-[max_cor max_index] = maxk(lower_triangle(:),1);
+[max_cor, max_index] = maxk(lower_triangle(:),1);
 [row, col] = ind2sub(size(mean_removed_matrix), max_index);
 settlement_names_max_between = [settlement_names(row); settlement_names(col)];
 fprintf('settlements with highest correlation:\n');
 fprintf('%s\n', settlement_names_max_between);
 
-[min_corr min_index] = min(lower_triangle(:));
+[min_corr, min_index] = min(lower_triangle(:));
 [row, col] = ind2sub(size(mean_removed_matrix), min_index);
 settlement_names_min_between = [settlement_names(row); settlement_names(col)];
 fprintf('settlements with lowest correlation:\n');
@@ -158,7 +158,7 @@ fprintf('%s\n', settlement_names_min_between);
 % Cluster the voting data to groups according to voting pattern
 % a: samples and features
 samples_settelments = raw_votes;
-num_samples_settelments = size(samples_settelments,1);
+num_samples_settelments = size(samples_settelments,1); 
 % Num. of samples (settelments): 1214
 num_features_voting = size(samples_settelments,2);
 % Num. of features (parties): 30
@@ -167,89 +167,47 @@ num_features_voting = size(samples_settelments,2);
 % Calculate kmeans for each distance type,
 % using kmeans (k=2:10) with 10 replicates
 
+% TODO: create 1 loop for 3 figures
 % kmeans clustering, distance type: sqeuclidean
-k_options = 2:10;
-silh_avg_all = zeros(size(k_options));
-figure;
-for ii_k = 1:length(k_options)
-    rng(0); % For reproducibility
-    subplot(5,2,ii_k);
-    k = k_options(ii_k);
-    [cidx,cmeans] = kmeans(samples_settelments, k,'distance',...
-        'sqeuclidean','replicates', 10);
-    [silh,h] = silhouette(samples_settelments,cidx,'sqeuclidean');
-    silh_avg_all(ii_k) = mean(silh);
-    title(sprintf('k = %d',k));
-    sgtitle('Silhouette value for different clusters number (distance=sqeuclidean)');
+min_k = 2;
+max_k = 10;
+k_options = min_k:max_k; 
+silh_avg_all = zeros(size(k_options)); 
+distance_type = {'sqeuclidean', 'cosine', 'correlation'};
+num_replicates = 10;
+num_of_rows = 5;
+num_of_col = 2;
+x_axis_indent = 0.25;
+y_axis_indent = 0.01;
+for i_distance = 1:length(distance_type)
+    figure;
+    for ii_k = 1:length(k_options)
+        rng(0); % For reproducibility
+        subplot(num_of_rows,num_of_col,ii_k); 
+        k = k_options(ii_k);
+        [cidx,~] = kmeans(samples_settelments, k,'distance',...
+            distance_type(i_distance),'replicates', num_replicates);
+        [silh,~] = silhouette(samples_settelments,cidx,distance_type{i_distance});
+        silh_avg_all(ii_k) = mean(silh);
+        title(sprintf('k = %d',k));
+        title_text = ['silhouette value with different cluster number (k), distance: '...,
+            distance_type{i_distance}]; 
+        sgtitle(title_text);
+    end
+    subplot(num_of_rows,num_of_col,length(k_options)+1); 
+    hold on
+    [max_silh,max_silh_idx] = max(silh_avg_all); 
+    % find the optimal k (with maximal silhouette value)
+    plot(k_options, silh_avg_all);
+    plot(k_options(max_silh_idx), max_silh,'*r');
+    txt = sprintf('k = %d',k_options(max_silh_idx));
+    text(k_options(max_silh_idx) + x_axis_indent, ...
+        max_silh + y_axis_indent, txt); 
+    ylabel('Silhouette value');
+    xlabel('k');
+    title('Silhouette value per k');
+    hold off
 end
-subplot(5,2,length(k_options)+1);
-hold on
-[max_silh,max_silh_IX] = max(silh_avg_all);
-% find the optimal k (with maximal silhouette value)
-plot(k_options, silh_avg_all)
-plot(k_options(max_silh_IX), max_silh,'*r')
-txt = sprintf('k = %d',k_options(max_silh_IX));
-text(k_options(max_silh_IX)+0.25, max_silh+0.01,txt);
-ylabel('Silhouette value');
-xlabel('k');
-title('Silhouette value per k');
-hold off
-
-% kmeans clustering, distance type: cosine
-k_options = 2:10;
-silh_avg_all = zeros(size(k_options));
-figure
-for ii_k = 1:length(k_options)
-    rng(0); % For reproducibility
-    subplot(5,2,ii_k);
-    k = k_options(ii_k);
-    [cidx,cmeans] = kmeans(samples_settelments, k,'distance',...
-        'cosine','replicates', 10);
-    [silh,h] = silhouette(samples_settelments,cidx,'cosine');
-    silh_avg_all(ii_k) = mean(silh);
-    title(sprintf('k = %d',k));
-    sgtitle('Silhouette value for different clusters number (distance=cosine)');
-end
-subplot(5,2,length(k_options)+1);
-hold on
-[max_silh,max_silh_IX] = max(silh_avg_all);
-% find the optimal k (with maximal silhouette value)
-plot(k_options, silh_avg_all);
-plot(k_options(max_silh_IX), max_silh,'*r');
-txt = sprintf('k = %d',k_options(max_silh_IX));
-text(k_options(max_silh_IX)+0.25, max_silh+0.01,txt);
-ylabel('Silhouette value');
-xlabel('k');
-title('Silhouette value per k');
-hold off
-
-% kmeans clustering, distance type: correlation
-k_options = 2:10;
-silh_avg_all = zeros(size(k_options));
-figure
-for ii_k = 1:length(k_options)
-    rng(0); % For reproducibility
-    subplot(5,2,ii_k);
-    k = k_options(ii_k);
-    [cidx,cmeans] = kmeans(samples_settelments, k,'distance',...
-        'correlation','replicates', 10);
-    [silh,h] = silhouette(samples_settelments,cidx,'correlation');
-    silh_avg_all(ii_k) = mean(silh);
-    title(sprintf('k = %d',k));
-    sgtitle('Silhouette value for different clusters number (distance=correlation)');
-end
-subplot(5,2,length(k_options)+1);
-hold on
-[max_silh,max_silh_IX] = max(silh_avg_all);
-% find the optimal k (with maximal silhouette value)
-plot(k_options, silh_avg_all);
-plot(k_options(max_silh_IX), max_silh,'*r');
-txt = sprintf('k = %d',k_options(max_silh_IX));
-text(k_options(max_silh_IX)+0.25, max_silh+0.01,txt);
-ylabel('Silhouette value');
-xlabel('k');
-title('Silhouette value per k');
-hold off
 
 % g: Can you explain why using the 'sqeuclidean' created different results?
 % How can you fix it?
@@ -259,7 +217,9 @@ hold off
 % between them (i.e. Tel-Aviv with 268116 votes vs. Amuka with 97 votes)
 % that can bias the results of the Euclidaen distances calculation.
 % therefore, cosine/correlation
-% methods should be used to fix this bias.
+% methods should be used to fix this bias. Alternatively, we can normalize
+% the features according to their size so that the magnitude of the vectors
+% would be similar, regardless of the settelment size. 
 
 %% Q5.
 % clustering results with optimal k (k=5), using 'correlation' distance 
@@ -270,6 +230,8 @@ rng(0); % For reproducibility
 k = 5;
 [cidx,cmeans] = kmeans(samples_settelments, k,'distance',...
     'correlation','replicates', 10);
+num_of_rows = 2;
+num_of_col = 2;
 
 % Pie chart showing the percentage of data points in each cluster
 num_clusters = zeros(1,k);
@@ -278,7 +240,7 @@ for cluster = 1:k
 end
 figure;
 set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
-subplot(2,2,1);
+subplot(num_of_rows,num_of_col,1);
 pie(num_clusters);
 colormap([0 0 1; 1 0 0; 1 1 0; 0 1 1; 0 1 0]);
 labels = {'Cluster 1','Cluster 2','Cluster 3','Cluster 4','Cluster 5'};
@@ -299,7 +261,7 @@ for cluster = 1:k
 end
 
 % Plot the figure 
-subplot(2,2,2);
+subplot(num_of_rows,num_of_col,2);
 stem(percentage_votes_cluster(1,:),'b');
 hold on
 stem(percentage_votes_cluster(2,:),'r');
@@ -310,9 +272,9 @@ plot(percentage_votes,'k');
 xticks(1:num_features_voting);
 xticklabels(party_names_spaces);
 xtickangle(90);
-ylabel('voting percentages (%)');
-xlabel('parties');
-title('voting pattern in each cluster and in the general population','fontsize',14);
+ylabel('Voting percentages (%)');
+xlabel('Parties');
+title('Voting pattern in each cluster and in the general population','fontsize',14);
 legend('Cluster 1','Cluster 2','Cluster 3','Cluster 4','Cluster 5',...
     'General voting','Fontsize',8,'Location','northeast','Orientation','vertical');
 
@@ -331,7 +293,7 @@ correlations_cluster4 = correlation_matrix(end,idx_cluster4);
 correlations_cluster5 = correlation_matrix(end,idx_cluster5);
 
 % plot histogram
-subplot(2,2,3);
+subplot(num_of_rows,num_of_col,3);
 histogram(correlations_cluster1,'Facecolor','b');
 hold on
 histogram(correlations_cluster2,'Facecolor','r');
@@ -339,17 +301,17 @@ histogram(correlations_cluster3,'Facecolor','y');
 histogram(correlations_cluster4,'Facecolor','c');
 histogram(correlations_cluster5,'Facecolor','g');
 hold off
-title('correlation values of each cluster','Fontsize',14);
+title('Correlation values of each cluster','Fontsize',14);
 legend(labels,'Location','northeastoutside','Orientation','vertical');
-xlabel('correlation values of each cluster with the general voting pattern');
-ylabel('count');
+xlabel('Correlation values of each cluster with the general voting pattern');
+ylabel('Count');
 
 % plot a 3D graph, with the following axes: Number of votes, 
 % Voting rate (in percentage), Correlation to the total voting pattern.
 num_votes_per_settelment = table2array(results(:,4));
 voting_rate_per_settelment = table2array(results(:,4))./table2array(results(:,3))*100;
 cor_to_total_voting_pattern = correlation_matrix(end,1:(end-1));
-subplot(2,2,4);
+subplot(num_of_rows,num_of_col,4);
 plot3(num_votes_per_settelment(idx_cluster1),...
     voting_rate_per_settelment(idx_cluster1),...
     cor_to_total_voting_pattern(idx_cluster1),'o','Color','b');
@@ -367,9 +329,9 @@ plot3(num_votes_per_settelment(idx_cluster5),...
     voting_rate_per_settelment(idx_cluster5),...
     cor_to_total_voting_pattern(idx_cluster5),'o','Color','g');
 hold off
-xlabel('number of votes per settelment');
-ylabel('voting rate per settelment');
-zlabel('correlation to total voting pattern');
+xlabel('Number of votes per settelment');
+ylabel('Voting rate per settelment');
+zlabel('Correlation to general voting');
 title('3D plot of the clusters','Fontsize',14);
 legend(labels,'Location','northeast','Orientation','vertical');
 view(-37,30); %define the camera view
